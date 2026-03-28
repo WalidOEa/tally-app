6c9a9af1fd8a5d862b467e57e56045ee359f9ecd
WalidOEa <welamriti57@gmail.com>
2026-03-28 20:18:23 +0000

    Add animation to hamburger

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import {useEffect, useState} from 'react';
+import {useEffect, useState, useRef} from 'react';
 import './App.css';
 import {Increment, Decrement, GetInitialCount} from "../wailsjs/go/main/App";
 import hamburger from './assets/animations/hamburger.json';
@@ -7,6 +7,7 @@ import {Player} from '@lottiefiles/react-lottie-player';
 function App() {
     const [count, setCount] = useState(0);
     const [sidebarOpen, setSidebarOpen] = useState(true);
+    const playerRef = useRef(null);
 
     useEffect(() => {
         GetInitialCount().then(result => {
@@ -22,14 +23,33 @@ function App() {
         Decrement().then(setCount)
     }
 
+    function toggleSidebar() {
+        setSidebarOpen(o => !o);
+    }
+
     return (
         <div id="App" className="app-container">
             <aside className={`sidebar ${sidebarOpen ? '' : 'sidebar-collapsed'}`}>
-                <button className="sidebar-toggle" onClick={() => setSidebarOpen(o => !o)}>
+                <button 
+                    className="sidebar-toggle" 
+                    onClick={toggleSidebar}
+                    onMouseEnter={() => {
+                        const player = playerRef.current;
+                        if (player) {
+                            player.setSeeker(0);
+                            player.play();
+                        }
+                    }}>
                     <Player
+                        ref={playerRef}
                         autoplay={false}
+                        loop={false}
                         src={hamburger}
-                        style={{ height: '24px', width: '24px' }}
+                        style={{ 
+                            height: '24px', 
+                            width: '24px',
+                            transform: sidebarOpen ? 'rotate(0deg)' : 'rotate(180deg',
+                        }}
                     />
                 </button>
                 {sidebarOpen && (
